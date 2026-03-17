4cbd745a649132b23d456bcb21ac935226e85ef5
Aidan Mahoney <149884417+aidanmahoney@users.noreply.github.com>
2026-03-16 19:26:17 -0500

    fix: correct base name in App.jsx

--- src/components/structural/App.jsx
+++ src/components/structural/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { HashRouter, Route, Routes } from 'react-router';
 
 function App() {
-    <BrowserRouter basename="/aidanmahoney.github.io/">
+    <BrowserRouter basename="/p170/">
         <Routes>
             {/* TODO: add routes */}
         </Routes>
