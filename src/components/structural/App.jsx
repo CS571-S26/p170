4ea1d66f165e4cf077eff559a4d983a6b0938cda
Aidan Mahoney <149884417+aidanmahoney@users.noreply.github.com>
2026-03-16 20:01:08 -0500

    Wrap HashRouter with return statement

--- src/components/structural/App.jsx
+++ src/components/structural/App.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { HashRouter, Route, Routes } from 'react-router';
 
 function App() {
-    <HashRouter>
-        <h1>Wisconsin Neuromorphic Computing & NeuroAI Lab</h1>
-        <Routes>
-            {/* TODO: add routes */}
-        </Routes>
-    </HashRouter>
+    return (
+        <HashRouter>
+            <h1>Wisconsin Neuromorphic Computing & NeuroAI Lab</h1>
+            <Routes>
+                {/* TODO: add routes */}
+            </Routes>
+        </HashRouter>
+    )
 }
 
 export default App;
